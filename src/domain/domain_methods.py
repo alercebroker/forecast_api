8adad8804b2c6f618b2011c557d6b5236e376400
mcaceres2017 <matias.guido.caceres1998@gmail.com>
2023-02-22 10:58:22 -0300

    methods "rename get fit" and "params to df" added

--- src/domain/domain_methods.py
+++ src/domain/domain_methods.py
@@ -1,6 +1,5 @@
 from astropy.time import Time
 import datetime
-from alerce.core import Alerce
 from alerce.exceptions import ObjectNotFoundError
 from flask_restx import abort
 from src.domain.SN_model import SNModel, MODEL_PARAMS
@@ -34,6 +33,23 @@ def fit_parameters(oid, client, extractor):
     return params
 
 
+def rename_and_get_fit(params):
+    fids = [int(i.rsplit("_", 1)[1]) for i in params.index]
+    params.index = [i.rsplit("_", 1)[0] for i in params.index]
+    return fids
+
+
+def params_to_dataframe(params, fids):
+    params = params.to_frame()
+    params.reset_index(inplace=True)
+    # Setting new column names
+    params.columns = ["name", "value"]
+
+    # Adding fid
+    params["fid"] = fids
+    return params
+
+
 def get_parameters(oid, client):
     features = client.query_features(oid, format="pandas")
     if len(features) > 0:
@@ -44,17 +60,11 @@ def get_parameters(oid, client):
         params = fit_parameters(oid).iloc[0]
 
         # Renaming index and getting fid
-        fids = [int(i.rsplit("_", 1)[1]) for i in params.index]
-        params.index = [i.rsplit("_", 1)[0] for i in params.index]
+        fids = rename_and_get_fit(params)
 
         # Transforming to dataframe and resetting index
-        params = params.to_frame()
-        params.reset_index(inplace=True)
-        # Setting new column names
-        params.columns = ["name", "value"]
+        params = params_to_dataframe(params, fids)
 
-        # Adding fid
-        params["fid"] = fids
         return False, params
 
 
