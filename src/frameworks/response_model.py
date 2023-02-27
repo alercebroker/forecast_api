7330602321773fb094deb59cb6f8811cb74e7681
mcaceres2017 <matias.guido.caceres1998@gmail.com>
2023-02-27 14:40:28 -0300

    response models updated

--- src/frameworks/response_model.py
+++ src/frameworks/response_model.py
@@ -1,19 +1,15 @@
-from flask_restx import fields, Model
+from pydantic import BaseModel
+from fastapi import Query
+from typing import List
 
-forecast_model = Model(
-    "forecast",
-    {
-        "magpsf": fields.List(fields.Float(description="Forecast Apparent Magnitude")),
-        "mjd": fields.List(fields.Float(description="MJD associated to forecast")),
-        "fid": fields.Integer(description="Filter ID (1=g; 2=r; 3=i)"),
-    },
-)
 
-parametric_response = Model(
-    "Parametric Model Response",
-    {
-        "oid": fields.String(description="Object identifier"),
-        "forecast": fields.List(fields.Nested(forecast_model)),
-        "comment": fields.String(description="Metadata from forecast"),
-    },
-)
+class ForecastModel(BaseModel):
+    magpsf: List[float] = Query(description="Forecast Apparent Magnitude")
+    mjd: List[float] = Query(description="MJD associated to forecast")
+    fid: int = Query(description="Filter ID (1=g; 2=r; 3=i)")
+
+
+class ParametricResponse(BaseModel):
+    oid: str = Query(description="Object identifier")
+    forecast: List[ForecastModel]
+    comment: str = Query(description="Metadata from forecast")
